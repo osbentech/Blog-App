efe7a24f3f0be7679f28b15f559048cdabfcfd52
Osegbo Benjamin Ejike <benmma13@gmail.com>
2023-03-04 19:11:22 +0400

    add validations for name postcounter title like andcomment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,21 @@
 class Post < ApplicationRecord
-  has_many :comments, foreign_key: :posts_id
-  has_many :likes, foreign_key: :posts_id
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  after_save :update_posts_counter
-
-  def latest_comments
-    comments.order(created_at: :desc).limit(5)
+    has_many :comments, foreign_key: :posts_id
+    has_many :likes, foreign_key: :posts_id
+    belongs_to :author, class_name: 'User', foreign_key: :author_id
+    after_save :update_posts_counter
+    validates :title, presence: true, length: { maximum: 250 }
+    validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  
+    def latest_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    private
+  
+    def update_posts_counter
+      author.increment(:posts_counter)
+    end
   end
-
-  private
-
-  def update_posts_counter
-    author.increment(:posts_counter)
-  end
-end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  has_many :comments, foreign_key: :author_id
-  has_many :posts, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-
-  def latest_posts
-    posts.order(created_at: :desc).limit(3)
+    has_many :comments, foreign_key: :author_id
+    has_many :posts, foreign_key: :author_id
+    has_many :likes, foreign_key: :author_id
+    validates_presence_of :name
+    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+    def latest_posts
+      posts.order(created_at: :desc).limit(3)
+    end
   end
-end
