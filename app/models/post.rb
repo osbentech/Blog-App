4b1aca21110a1c1fbe8428921bd0b352dcf9d550
Osegbo Benjamin Ejike <benmma13@gmail.com>
2023-03-04 19:30:33 +0400

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-
   def latest_comments
     comments.order(created_at: :desc).limit(5)
   end
