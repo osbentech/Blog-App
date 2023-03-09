fc323fe3d549868b8caf51b55ce342bba972518a
Osegbo Benjamin Ejike <benmma13@gmail.com>
2023-03-09 22:53:50 +0400

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
   class PostsController < ApplicationController
     before_action :set_user
-  
+
     def index
       @posts = Post.all
     end
-  
+
     def show
       @post = Post.find(params[:id])
       puts @post.inspect
     end
-  
+
     def set_user
       @user = User.find(params[:user_id])
     end
   end
+end
