cb81e9058b475b175e7460725bcffd66ae12b43d
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-14 23:45:10 +0400

    clear some spaces

--- spec/view/post_index_spec.rb
+++ spec/view/post_index_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'post index page', type: :feature do
   before do
-    @user = User.create(name: 'Ben', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer',
-                        posts_counter: 1)
+    @user = User.create(name: 'Ben', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer', posts_counter: 1)
     @post = Post.create(title: 'ti', text: 'Great post', comments_counter: 2, likes_counter: 1, author: @user)
     @first_comment = Comment.create(text: 'First comment', author: @user, post: @post)
     @second_comment = Comment.create(text: 'Second comment', author: @user, post: @post)
     Like.create(author: @user, post: @post)
-
     visit user_posts_path(user_id: @user.id)
   end
 
@@ -43,7 +41,6 @@ RSpec.describe 'post index page', type: :feature do
   it 'should displays the number of likes' do
     expect(page).to have_content(@post.likes_counter)
   end
-
   it 'should displays the pagenation button' do
     expect(page).to have_content('Pagination')
   end
