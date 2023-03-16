54355f74d46fa39f575383eaab7750c94fe66ad0
Osegbo Benjamin Ejike <benmma13@gmail.com>
2023-03-16 23:31:04 +0400

    Add device setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
@@ -9,6 +13,6 @@ class User < ApplicationRecord
   validates :photo, presence: true, length: { maximum: 250 }
 
   def latest_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.includes(:author).order(created_at: :desc).limit(3)
   end
 end
