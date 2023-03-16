d7f1ea7644d9f10c12a08410a8d35ea17261c02e
Osegbo Benjamin Ejike <benmma13@gmail.com>
2023-03-17 03:17:37 +0400

    Add device setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,10 +33,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   validates :name, presence: true, length: { maximum: 30 }
-  validates :bio, presence: true, length: { maximum: 100 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :posts_counter, numericality: { only_integer: true, less_than_or_equal_to: 100 }
-  validates :photo, presence: true, length: { maximum: 250 }
 
   def latest_posts
     posts.includes(:author).order(created_at: :desc).limit(3)
